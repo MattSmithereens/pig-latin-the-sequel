36fd07bc943702e37a75ccd2da18f1fd98a40d5b
Abel Trotter and Matt Smith <matt@>
2018-06-05 16:32:32 -0700

    finished backend.  factorial doesnt display yet

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
+function runFactorial(input) {
+  if (input === 0 || input === 1) return 1;
+  for(var i = input - 1; i >= 1; i--) {
+    input = input * i;
+    console.log(input);
+  }
+  return input;
+}
+
 $(document).ready(function() {
   $("#submit").click(function(e) {
     e.preventDefault();
+
+    var inputtedNumber = parseInt($("#input").val());
+    runFactorial(inputtedNumber);
   });
 });
