68d0c5a064d6af0383f47edb040240088d0228b3
Annie Shin and Matt Smith <annie@>
2018-06-06 14:52:07 -0700

    add Js for words that start with qu and QU to return quay at the end of the word

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,24 @@
 function pigLatin(word) {
   var vowels = ["a","e","i","o","u","A","E","I","O","U"];
   var vowelsY = ["a","e","i","o","u","y","A","E","I","O","U","Y"];
-    result = word.split("");
+  var lettersQ = ["qu","Qu"];
+  var result = word.split("");
 
-  if (vowels.includes(word[0])) {
+  console.log(lettersQ[1] + " " + lettersQ[0]);
+
+  if (word.includes(lettersQ[0]) || word.includes(lettersQ[1])) {
+    result.shift();
+    result.shift();
+    result.push('quay');
+  // } else {
+    // return result.join("");
+  }
+
+
+   else if (vowels.includes(word[0])) {
     return word += "way";
   } else {
     for (var i = 0; i < word.length; i++) {
-// test for consonants
       if (!vowels.includes(word[i])) {
         result.push(result.shift());
       } else {
@@ -17,19 +28,17 @@ function pigLatin(word) {
       }
     }
   }
+
   if (vowelsY.includes(word[5,11])) {
     return word += "yay";
   } else {
     return result.join("");
   }
-  // if (vowelsY.includes(word[5,11])) {
-  //   return word += "yay";
-  // } else {
-  //   return result.join("");
-  // }
+
+
 }
 
 
 
 
-console.log(pigLatin("yesterday"));
+console.log(pigLatin("qatar"));
